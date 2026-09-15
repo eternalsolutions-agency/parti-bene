e94823e1f8ef2fdf117c987f7f79aeca0a157a3d
eternalsolutions-agency <ricandsmm@gmail.com>
2026-09-15 15:28:24 +0000

    Collega tabella assegnazioni richieste

--- appwrite-config.js
+++ appwrite-config.js
@@ -16,6 +16,7 @@ export const APPWRITE_CONFIG = Object.freeze({
   applicationsTableId: "6aa8e72c0018ffbfe9f5",
   offersTableId: "6aa9582c00030c21a118",
   travelRequestsTableId: "6aa96117003d70271c30",
+  requestAssignmentsTableId: "6aa963d800048778c6e3",
   mediaBucketId: "6aa8ea22003dda7c03ca"
 });
 
